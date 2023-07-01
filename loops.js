a061a2fe8c60ed1ee7f5d81491304c48915de527
MelroyL92 <melroy_lingsma@live.nl>
2023-07-01 07:44:22 +0200

    Loops opdracht + loops and arrays bijna af

--- loops.js
+++ loops.js
@@ -10,7 +10,9 @@
 // Tip: je kunt de .repeat() methode gebruiken om een karakter een n aantal keer te herhalen...
 // Dit heb je nog niet geleerd, maar bekijk hiervoor dit MDN-artikel eens: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat
 // ==========================================
-
+for (let i = 1; i <=5 ; i++) {
+    console.log("*".repeat(i));
+}
 
 
 
@@ -25,7 +27,14 @@
 // klaar!
 // ==========================================
 
+for (let i = 0; i <5; i++) {
 
+    if (i <4) {
+        console.log("loop...")
+    } else {
+        console.log("klaar!")
+    }
+}
 
 
 
@@ -45,6 +54,16 @@
 // >> 9
 // ==========================================
 
+for (let i = 0; i <10 ; i++) {
+
+    if (i < 3) {
+        console.log (i);
+    } else if (i < 6) {
+        console.log (">" + i);
+    } else {
+        console.log (">>" + i)
+    }
+}
 
 
 
@@ -87,5 +106,16 @@
 // etc.
 // ==========================================
 
+for (let i = 1; i <= 100 ; i ++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log("FizzBuzz");
+    } else if (i % 5 === 0) {
+        console.log ("Buzz");
+    } else if (i % 3 === 0) {
+        console.log("Fizz");
+    } else {
+        console.log(i)
+    }
 
+}
 
